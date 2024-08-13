c5d0431d78cbc7980f008151ce970b9041769bfc
khushboo kanojiya <khushboo@marketsmojo.com>
2024-08-13 11:47:06 +0530

    JS object started

--- Java_script/object.js
+++ Java_script/object.js
@@ -1,1 +1,12 @@
 console.log('welcome object')
+let schoole = {
+  name: 'aiktc',
+  location: 'delhi',
+  established: '1971',
+  displayInfo: function () {
+    console.log(`${schoole.name} was established in ${schoole.established} at ${schoole.location}`);
+  }
+
+
+}
+schoole.displayInfo();
